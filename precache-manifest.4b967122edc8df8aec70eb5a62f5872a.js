f0979b2a2ca2a0993dd08837d4e10846f2a21061
grhgrmgrhrm <grhgrmgrhrm@gmail.com>
2021-07-03 16:45:24 +0300

    feat: add new deploy to gh-pages after update

---
+++ precache-manifest.4b967122edc8df8aec70eb5a62f5872a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dea9961e189356200427",
+    "url": "/find-me-a-frontend-developer/static/css/main.39c4f1b6.chunk.css"
+  },
+  {
+    "revision": "dea9961e189356200427",
+    "url": "/find-me-a-frontend-developer/static/js/main.dea9961e.chunk.js"
+  },
+  {
+    "revision": "5a80405e8fff7c81f25f",
+    "url": "/find-me-a-frontend-developer/static/js/1.5a80405e.chunk.js"
+  },
+  {
+    "revision": "f7e70fc0eed93a2d019d",
+    "url": "/find-me-a-frontend-developer/static/js/runtime~main.f7e70fc0.js"
+  },
+  {
+    "revision": "84287d09b8053c6fa598893b8910786a",
+    "url": "/find-me-a-frontend-developer/static/media/logo.84287d09.svg"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/find-me-a-frontend-developer/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "c8894168d1c87096499b5e4f9f43e31c",
+    "url": "/find-me-a-frontend-developer/index.html"
+  }
+];
